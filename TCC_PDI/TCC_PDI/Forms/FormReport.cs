0bad4a5d0cc6ad8f6127f947ec66f6dd899d01dc
GiuElisa <giuliaelisapereiraa@gmail.com>
2023-09-06 08:22:05 -0300

    Estrutura do Relatório HTML aberto direto no WebBrowser 06/09

--- TCC_PDI/TCC_PDI/Forms/FormReport.cs
+++ TCC_PDI/TCC_PDI/Forms/FormReport.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Text;
 using System.Windows.Forms;
 
@@ -10,6 +11,8 @@ namespace TCC_PDI.Forms
 {
     public partial class FormReport : Form
     {
+        string html;
+
         public FormReport()
         {
             InitializeComponent();
@@ -26,14 +29,14 @@ namespace TCC_PDI.Forms
 
         private void abrirAba()
         {
+            gerarRelatorio();
             TabPage aba = new TabPage();
-            aba.Text = "Relatório";
             tabControl1.Controls.Add(aba);
             tabControl1.SelectTab(tabControl1.TabCount - 1);
             WebBrowser browser = new WebBrowser() { ScriptErrorsSuppressed = true };
             browser.Parent = aba;
             browser.Dock = DockStyle.Fill;
-            browser.Navigate("http://www.google.com");
+            browser.DocumentText = html;
             browser.DocumentCompleted += Browser_DocumentCompleted;
         }
 
@@ -42,6 +45,30 @@ namespace TCC_PDI.Forms
             WebBrowser browser = tabControl1.SelectedTab.Controls[0] as WebBrowser;
             if (browser != null)
                 tabControl1.SelectedTab.Text = browser.DocumentTitle;
+        }
+
+        private void gerarRelatorio()
+        {
+            //Inicia a Table
+            html = "<table cellpadding='5' cellspacing='0' style='border: 1px solid #ccc;font-size: 12pt;font-family:arial'>";
+
+            //Incluir o Header
+            html += "<tr>";
+            html += "<th style='background-color: #B8DBFD;border: 1px solid #ccc'>Nome</th>";
+            html += "</tr>";
+
+            //Adiciona uma linha.
+            html += "<tr>";
+            html += "<td style='width:120px;border: 1px solid #ccc'>Aí Messiiiiiiiiiiii</td>";
+            html += "</tr>";
+
+            //Fecha a table.
+            html += "</table>";
+
+            //Define o caminho e cria o arquivo HTML
+            var caminhoArquivoHTML = @"C:\Users\aluno\Desktop\Teste.html";
+
+            File.WriteAllText(caminhoArquivoHTML, html);
         }
 
         //private void btnRetorna_Click(object sender, EventArgs e)
